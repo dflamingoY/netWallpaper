4a04a8ec15ebce539a0044ece1071a9e63f83095
mingo <ym199210@163.com>
2020-12-15 19:13:59 +0800

    添加3d 球体

--- app/src/main/java/com/gaqiujun/moment1/module/search/SearchActivity.kt
+++ app/src/main/java/com/gaqiujun/moment1/module/search/SearchActivity.kt
@@ -1,6 +1,9 @@
 package com.gaqiujun.moment1.module.search
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.text.TextUtils
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -10,18 +13,20 @@ import com.gaqiujun.moment1.entity.SearchTagBean
 import com.gaqiujun.moment1.injection.component.DaggerSearchActComponent
 import com.gaqiujun.moment1.module.search.presenter.SearchPresenter
 import com.gaqiujun.moment1.module.search.view.SearchWallpaperView
+import com.gaqiujun.moment1.widget.dTag.TagsAdapter
 import com.mingo.baselibrary.base.BaseMvpAct
-import com.moxun.tagcloudlib.view.TagsAdapter
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : BaseMvpAct<SearchPresenter>(), SearchWallpaperView {
     private lateinit var tagAdapter: TagsAdapter
     private val mData by lazy { ArrayList<SearchTagBean>() }
+
     override fun getLayoutId(): Int {
         return R.layout.activity_search
     }
 
     override fun initView() {
+        setTransparentStatusBar()
         DaggerSearchActComponent.builder().activityComponent(actComponent).build().inject(this)
         presenter.view = this
     }
@@ -35,6 +40,25 @@ class SearchActivity : BaseMvpAct<SearchPresenter>(), SearchWallpaperView {
             override fun getView(context: Context?, position: Int, parent: ViewGroup?): View {
                 val tv = TextView(this@SearchActivity)
                 tv.text = mData[position].title
+                tv.textSize = 10f
+                val mGd = GradientDrawable()
+                if (!TextUtils.isEmpty(mData[position].color)) {
+                    val colors = mData[position].color?.split(",")
+                    mGd.setColor(
+                        Color.parseColor(
+                            "#" + Integer.toHexString(
+                                colors!![0].toInt()
+                            ) + Integer.toHexString(colors[1].toInt()) + Integer.toHexString(
+                                colors[2].toInt()
+                            )
+                        )
+                    )
+                } else {
+                    mGd.setColor(Color.WHITE)
+                }
+                tv.setPadding(10, 5, 10, 5)
+                mGd.cornerRadius = 10.0f
+                tv.background = mGd
                 return tv
             }
 
@@ -43,15 +67,14 @@ class SearchActivity : BaseMvpAct<SearchPresenter>(), SearchWallpaperView {
             }
 
             override fun getPopularity(position: Int): Int {
-                return 10
+                return 2
             }
 
-            override fun onThemeColorChanged(view: View?, themeColor: Int) {
-
+            override fun onThemeColorChanged(view: View?, themeColor: Int, alpha: Float) {
             }
-
         }
         tagCloudView.setAdapter(tagAdapter)
+        tagCloudView.setManualScroll(false)
         presenter.getSearchTag()
     }
 
